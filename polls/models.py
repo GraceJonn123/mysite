2b714e6688ecb7ad1dca63181b405d98ea550524
GraceJonn123 <gj.gjonn@gmail.com>
2016-03-26 01:00:54 +0300

    adding model chamges

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -5,6 +7,9 @@ from django.utils.encoding import python_2_unicode_compatible
 class Question(models.Model):
 	def __str__(self):
 		return self.question_text
+
+	def was_published_recently(self):
+		 return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
